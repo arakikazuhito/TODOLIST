159f4d29ff149ecd8c22d735caa0674aaa4e4948
かずひt <kazuhiton@kazuhitonnonotopasokon.local>
2023-06-25 19:59:22 +0900

    フォルダ一覧機能

---
+++ app/Models/Folder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Folder extends Model
+{
+        /**
+     * モデルに関連付けるテーブル
+     *
+     * @var string
+     */
+    protected $table = 'folders';
+
+}
