3f579d17106e9b0ce73c5550fdbe9cd65cde6bbd
かずひt <kazuhiton@kazuhitonnonotopasokon.local>
2023-06-26 22:30:05 +0900

    タスク作成機能

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/folders/{id}/tasks', [TaskController::class,'index'])->name('tasks.index');
+Route::get('/folders/{id}/tasks', [TaskController::class, 'index'])->name('tasks.index');
 
-Route::get('/folders/create', [FolderController::class,'showCreateForm'])->name('folders.create');
-Route::post('/folders/create', [FolderController::class,'create']);
+Route::get('/folders/create', [FolderController::class, 'showCreateForm'])->name('folders.create');
+Route::post('/folders/create', [FolderController::class, 'create']);
+
+Route::get('/folders/{id}/tasks/create', [TaskController::class, 'showCreateForm'])->name('tasks.create');
+Route::post('/folders/{id}/tasks/create', [TaskController::class, 'create']);
