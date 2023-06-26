7bfa9b4617036d57d0dc3a013b149c051ace3adf
かずひt <kazuhiton@kazuhitonnonotopasokon.local>
2023-06-26 23:39:11 +0900

    タスク編集機能

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -14,6 +14,8 @@ class TaskTest extends TestCase
 
     /**
      * 各テストメソッドの実行前に呼ばれる
+     *
+     *
      */
     public function setUp(): void
     {
@@ -23,9 +25,11 @@ class TaskTest extends TestCase
         $this->seed('FoldersTableSeeder');
     }
 
-    /**
+
+    /*
      * 期限日が日付ではない場合はバリデーションエラー
      * @test
+     * 
      */
     public function due_date_should_be_date()
     {
@@ -34,14 +38,32 @@ class TaskTest extends TestCase
         $response->assertSessionHasErrors(['due_date' => '期限日 には日付を入力してください。',]);
     }
 
+
     /**
      * 期限日が過去日付の場合はバリデーションエラー
      * @test
+     *
      */
     public function due_date_should_not_be_past()
     {
         $response = $this->post('/folders/1/tasks/create', ['title' => 'Sample task', 'due_date' => Carbon::yesterday()->format('Y/m/d'),]);
 
         $response->assertSessionHasErrors(['due_date' => '期限日 には今日以降の日付を入力してください。',]);
+    }
+
+
+    public function status_should_be_within_defined_numbers()
+    {
+        $this->seed('TasksTableSeeder');
+
+        $response = $this->post('/folders/1/tasks/1/edit', [
+            'title' => 'Sample task',
+            'due_date' => Carbon::today()->format('Y/m/d'),
+            'status' => 999,
+        ]);
+
+        $response->assertSessionHasErrors([
+            'status' => '状態 には 未着手、着手中、完了 のいずれかを指定してください。',
+        ]);
     }
 }
